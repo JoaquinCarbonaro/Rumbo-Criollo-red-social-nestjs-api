43c1ff58db3492e62ae4dfc8ef21f295efb837ee
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-10-28 15:16:38 -0300

    fix: temporal

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  
+
+//comentario: imprime ip publica del servidor render
+async function showPublicIp() {
+  try {
+    const res = await fetch('https://api.ipify.org?format=json');
+    const data = await res.json();
+    console.log('IP PUBLICA DE RENDER:', data.ip);
+  } catch (err) {
+    console.error('error obteniendo ip:', err);
+  }
+}
+await showPublicIp();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
