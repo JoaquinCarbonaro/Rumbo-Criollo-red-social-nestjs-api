98919c3e09a48f02d2fe227e68aa32916da3b0c0
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-10-28 18:31:37 -0300

    add: fin pruebas de conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { HealthController } from './health.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
   ],
-  controllers: [AppController,HealthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
